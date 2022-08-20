78c2efc1a975da986fa94944bd9e826570ed4b28
lixixi89055465 <nanji1850094299@outlook.com>
2022-08-20 14:41:35 +0800

    * 503. 下一个更大元素 II

---
+++ src/a600/M636ExclusiveTime.java
@@ -0,0 +1,15 @@
+package a600;
+
+import java.util.List;
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+
+    }
+}
+
+public class M636ExclusiveTime {
+    public static void main(String[] args) {
+
+    }
+}
