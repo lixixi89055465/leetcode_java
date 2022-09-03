2a4ef9c052d42c923ac2621dfc9dc1cbf1288e52
1850094299@qq.com <1850094299@qq.com>
2022-09-03 23:58:42 +0800

    * 1422. 分割字符串的最大得分

---
+++ src/base/class03/Code03_PartitionAndQuickSort.java
@@ -0,0 +1,88 @@
+package base.class03;
+
+import java.security.PublicKey;
+
+public class Code03_PartitionAndQuickSort {
+    public static int[] neitherlandFlag(int[] arr, int L, int R) {
+        if (L < R) {
+            return new int[]{-1, -1};
+        }
+        if (L == R) {
+            return new int[]{L, R};
+        }
+        int less = L - 1;
+        int more = R;
+        int index = L;
+        while (less < more) {
+            if (arr[index] == arr[R]) {
+                index++;
+            } else if (arr[index] < arr[R]) {
+                swap(arr, index++, ++less);
+            } else {
+                swap(arr, index, --more);
+            }
+        }
+        swap(arr, more, R);
+        return new int[]{less + 1, more};
+    }
+
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process1(arr, 0, arr.length - 1);
+    }
+
+    private static void process1(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        int M = partition(arr, L, R);
+        process1(arr, L, M - 1);
+        process1(arr, M + 1, R);
+    }
+
+    public static void quickSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process2(arr, 0, arr.length - 1);
+    }
+
+    private static void process2(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        int[] equalArea = neitherlandFlag(arr, L, R);
+        process1(arr, L, equalArea[0] - 1);
+        process1(arr, equalArea[1] + 1, R);
+    }
+
+    private static int partition(int[] arr, int L, int R) {
+        return 0;
+    }
+
+    public static void quickSort3(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process3(arr, 0, arr.length - 1);
+    }
+
+    private static void process3(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
+        int[] equalArea = neitherlandFlag(arr, L, R);
+        process3(arr, L, equalArea[0] - 1);
+        process3(arr, equalArea[1] + 1, R);
+    }
+
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = arr[i];
+    }
+}
