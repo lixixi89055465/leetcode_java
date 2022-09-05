3ac26ba1ac9f9844283677fee794bd4d355c8711
nanji <1850094299@qq.com>
2022-09-05 09:59:43 +0800

    ˚

--- src/test/HasStatic.java
+++ src/test/HasStatic.java
@@ -3,7 +3,13 @@ package test;
 class HasStatic{
     private static int x = 100;
     public static void main(String args[ ]){
-
-        System.out.print(444);
+        HasStatic hs1 = new HasStatic();
+        hs1.x++;
+        HasStatic hs2 = new HasStatic();
+        hs2.x++;
+        hs1=new HasStatic();
+        hs1.x++;
+        HasStatic.x--;
+        System.out.println( "x=" +x);
     }
 }
