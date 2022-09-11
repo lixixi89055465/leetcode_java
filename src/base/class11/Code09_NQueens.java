660258694523232e6e58b2f92bec714465daa82f
1850094299@qq.com <1850094299@qq.com>
2022-09-11 21:03:43 +0800

    暴力 递归

--- src/base/class11/Code09_NQueens.java
+++ src/base/class11/Code09_NQueens.java
@@ -25,6 +25,37 @@ public class Code09_NQueens {
     }
 
     private static boolean isValid(int[] record, int i, int j) {
-        return false;
+        for (int k = 0; k < i; k++) {
+            if (j == record[k] || Math.abs(k - i) == Math.abs(j - record[k])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int num2(int n) {
+        if (n < 1 || n > 32) {
+            return 0;
+        }
+        int limit = n == 32 ? -1 : (1 << n) - 1;
+        return process2(limit, 0, 0, 0);
+    }
+
+    private static int process2(int limit, int colLim, int leftDiaLim, int rightDiaLim) {
+        if (colLim == limit) {
+            return 1;
+        }
+        int pos = limit & (~(colLim | leftDiaLim | rightDiaLim));
+        int mostRightOne = 0;
+        int res = 0;
+        while (pos != 0) {
+            mostRightOne = pos & (~pos + 1);
+            pos = pos - mostRightOne;
+            res += process2(limit, colLim | mostRightOne,
+                    (leftDiaLim | mostRightOne) << 1,
+                    (rightDiaLim | mostRightOne) >>> 1);
+
+        }
+        return res;
     }
 }
