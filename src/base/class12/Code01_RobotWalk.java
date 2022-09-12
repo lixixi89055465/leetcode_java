c119f698edd26c39f4227939c9c23e6da6006482
1850094299@qq.com <1850094299@qq.com>
2022-09-12 11:06:57 +0800

    暴力 递归

---
+++ src/base/class12/Code01_RobotWalk.java
@@ -0,0 +1,62 @@
+package base.class12;
+
+public class Code01_RobotWalk {
+
+    public static int ways1(int N, int M, int K, int P) {
+        if (N < 2 || K < 1 || M < 1 || M > N || P < 1 || P > N) {
+            return 0;
+        }
+        return walk(N, M, K, P);
+    }
+
+    private static int walk(int N, int cur, int rest, int P) {
+        if (rest == 0) {
+            return cur == P ? 1 : 0;
+        }
+        if (cur == 1) {
+            return walk(N, 2, rest - 1, P);
+        }
+        if (cur == N) {
+            return walk(N, N - 1, rest - 1, P);
+        }
+        return walk(N, cur - 1, rest - 1, P) + walk(N, cur + 1, rest - 1, P);
+    }
+
+    public static int waysCache(int N, int M, int K, int P) {
+        if (N < 2 || K < 1 || M < 1 || M > N || P < 1 || P > N) {
+            return 0;
+        }
+        int[][] dp = new int[N + 1][K + 1];
+        for (int row = 0; row < N + 1; row++) {
+            for (int col = 0; col < K + 1; col++) {
+                dp[row][col] = -1;
+            }
+        }
+        return walkCache(N, M, K, P, dp);
+    }
+
+    private static int walkCache(int N, int cur, int rest, int P, int[][] dp) {
+        if (dp[cur][rest] != -1) {
+            return dp[cur][rest];
+        }
+        if (rest == 0) {
+            dp[cur][rest] = cur == P ? 1 : 0;
+            return dp[cur][rest];
+        }
+        if (cur == 1) {
+            dp[cur][rest] = walkCache(N, 2, rest - 1, P, dp);
+            return dp[cur][rest];
+        }
+        if (cur == N) {
+            dp[cur][rest] = walkCache(N, N - 1, rest - 1, P, dp);
+            return dp[cur][rest];
+        }
+        dp[cur][rest] = walkCache(N, cur + 1, rest - 1, P, dp) +
+                walkCache(N, cur - 1, rest - 1, P, dp);
+        return dp[cur][rest];
+    }
+
+    public static void main(String[] args) {
+        return;
+    }
+}
