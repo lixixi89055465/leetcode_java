c72d796340d32f8d81d8eb03b118dd74124cdef1
1850094299@qq.com <1850094299@qq.com>
2022-09-17 21:18:02 +0800

    差分数组

---
+++ src/zuo/publicclass2020/class099/Test03.java
@@ -0,0 +1,52 @@
+package zuo.publicclass2020.class099;
+
+// 这里有 n 个航班，它们分别从 1 到 n 进行编号。
+// 有一份航班预订表 bookings ，
+// 表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi]
+// 意味着在从 firsti 到 lasti 
+//（包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。
+// 请你返回一个长度为 n 的数组 answer，里面的元素是每个航班预定的座位总数。
+// 测试链接 : https://leetcode.cn/problems/corporate-flight-bookings/
+public class Test03 {
+
+    public static int[] corpFlightBookings(int[][] bookings, int n) {
+        // 1 2 3 4 n
+        // 0 1 2 3 .. n n+1
+        int[] cnt = new int[n + 2];
+        for (int[] book : bookings) {
+            // start book[0]
+            // end   book[1]
+            // 票    book[2]
+            cnt[book[0]] += book[2];
+            cnt[book[1] + 1] -= book[2];
+        }
+        for (int i = 1; i < cnt.length; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = cnt[i + 1];
+        }
+        return ans;
+    }
+
+    public static int[] corpFlightBookings1(int[][] bookings, int n) {
+        int[] cnt = new int[n + 1];
+        for (int[] booking : bookings) {
+            cnt[booking[0]] += booking[2];
+            if (booking[1] != n) {
+                cnt[booking[1] + 1] -= booking[2];
+            }
+        }
+        for (int i = 1; i < cnt.length; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[][] bookings = new int[][]{{0, 3, 10}, {2, 4, 20}, {3, 6, 11}};
+        int n = 10;
+        System.out.println(corpFlightBookings1(bookings, 10));
+    }
+}
