18bebc2693e9ecc81d0a171f491aa92a59631bdd
nanji <1850094299@qq.com>
2022-10-12 23:09:59 +0800

    zuo

--- src/zuo/base/class07/CoinsWays.java
+++ src/zuo/base/class07/CoinsWays.java
@@ -16,7 +16,66 @@ public class CoinsWays {
         return ways;
     }
 
+    public static int way2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int N = arr.length;
+        int[][] dp = new int[N + 1][aim + 1];
+        dp[N][0] = 1;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                int ways = 0;
+                for (int zhang = 0; arr[index] * zhang <= rest; zhang++) {
+                    ways += dp[index + 1][rest - arr[index] * zhang];
+                }
+                dp[index][rest] = ways;
+            }
+        }
+        return dp[0][aim];
+    }
+
+    public static int way3(int[] arr, int aim) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int N = arr.length;
+        int[][] dp = new int[N + 1][aim + 1];
+        dp[N][0] = 1;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                dp[index][rest] = dp[index + 1][rest];
+                if (rest - arr[index] >= 0) {
+                    dp[index][rest] += dp[index][rest - arr[index]];
+                }
+            }
+        }
+        return dp[0][aim];
+    }
+
+    public static int[] generateRandomArray(int len, int max) {
+        int[] arr = new int[(int) (Math.random() * len) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * max) + 1;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
+        int len = 10;
+        int max = 10;
+        int testTime = 10000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(len, max);
+            int aim = (int) (Math.random() * 3 * max) + max;
+            if (way1(arr, aim) != way2(arr, aim)
+                    || way2(arr, aim) != way3(arr, aim)) {
+                System.out.println("oooops!");
+                break;
+            }
+
+        }
+
 
     }
 }
