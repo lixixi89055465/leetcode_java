cd914bb696915ca4177b626c2ccf226c5b997c7c
nanji <1850094299@qq.com>
2022-10-17 22:08:25 +0800

    zuo

--- src/zuo/intermediate/class01/Problem02_AppleMinBags.java
+++ src/zuo/intermediate/class01/Problem02_AppleMinBags.java
@@ -7,17 +7,35 @@ public class Problem02_AppleMinBags {
         }
         int bag6 = -1;
         int bag8 = -1;
-        int rest = apple % bag8;
-        bag8 = apple / bag8;
+        int rest = apple % 8;
+        bag8 = apple / 8;
         while (bag8 >= 0 && rest < 24) {
             int rest6 = rest % 6 == 0 ? rest / 6 : -1;
-            if (rest != -1) {
+            if (rest6 != -1) {
                 bag6 = rest6;
                 break;
             }
             rest = apple - 8 * (--bag8);
         }
         return bag6 == -1 ? -1 : bag6 + bag8;
+    }
+
+    public static int minBagAwesome(int apple) {
+        if ((apple & 1) != 0) {
+            return -1;
+        }
+        if (apple < 18) {
+            return apple == 0 ? 0 : (apple == 6 || apple == 8) ? 1 :
+                    (apple == 12 || apple == 14 || apple == 16) ? 2 : -1;
+        }
+        return (apple - 18) / 8 + 3;
+    }
+
+    public static void main(String[] args) {
+        for (int apple = 0; apple < 100; apple++) {
+            System.out.println(apple + " : " + minBags(apple));
+        }
+//        minBags(90);
     }
 }
 
---
+++ src/zuo/intermediate/class02/Problem05_Eat.java
@@ -0,0 +1,37 @@
+package zuo.intermediate.class02;
+
+/**
+ * 动物吃草问题
+ */
+public class Problem05_Eat {
+    public static String winner1(int n) {
+        int base = 1;
+        if (n < 5) {
+            return n == 0 || n == 2 ? "后手" : "先手";
+        }
+        while (base <= n) {
+            if (winner1(n - base).equals("后手")) {
+                return "先手";
+            }
+            if (base > n / 4) {
+                break;
+            }
+            base *= 4;
+        }
+        return "后手";
+    }
+
+    public static String winner2(int n) {
+        if (n % 5 == 0 || n % 5 == 2) {
+            return "后手";
+        } else {
+            return "先手";
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(i + " : " + winner1(i));
+        }
+    }
+}
