eb9d9dd86cf817ae853a72c98419260da60802ec
nanji <1850094299@qq.com>
2022-10-18 19:18:44 +0800

    zuo

---
+++ src/zuo/intermediate/class02/AddProblem01.java
@@ -0,0 +1,20 @@
+package zuo.intermediate.class02;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 请保证所有字符串都是小写字母组成
+ */
+public class AddProblem01 {
+//    public static List<List<String>> getGroups(String[] strArr) {
+//        HashMap<Integer, List<Integer>> map = new HashMap<>();
+//        for (int i = 0; i < strArr.length; i++) {
+//
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/zuo/intermediate/class02/AddProblem02_MagicOP.java
@@ -0,0 +1,12 @@
+package zuo.intermediate.class02;
+
+/**
+ * 请保证所有字符串都是小写字母组成
+ */
+public class AddProblem02_MagicOP {
+
+
+    public static void main(String[] args) {
+
+    }
+}
