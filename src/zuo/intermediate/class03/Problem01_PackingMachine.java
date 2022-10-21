9199ddbe60a542bbf5606216eab4e5055fcc4cbe
lixixi89055465 <nanji1850094299@outlook.com>
2022-10-22 00:36:36 +0800

    * 886. Possible Bipartition

--- src/zuo/intermediate/class03/Problem01_PackingMachine.java
+++ src/zuo/intermediate/class03/Problem01_PackingMachine.java
@@ -1,6 +1,34 @@
 package zuo.intermediate.class03;
 
 public class Problem01_PackingMachine {
+    public static int MinOps(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int size = arr.length;
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
+        }
+        if (sum % size != 0) {
+            return -1;
+        }
+        int avg = sum / size;
+        int leftSum = 0;
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int leftRest = leftSum - i * avg;
+            int rightRest = (sum - leftRest - arr[i]) - (size - i - 1) * avg;
+            if (leftRest < 0 && rightRest < 0) {
+                ans = Math.max(ans, Math.abs(leftRest) + Math.abs(rightRest));
+            } else {
+                ans = Math.max(ans, Math.max(Math.abs(leftRest), Math.abs(rightRest)));
+            }
+            leftSum += arr[i];
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
 
