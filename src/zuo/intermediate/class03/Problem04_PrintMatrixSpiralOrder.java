855f11ebdaeedb6a24ff93d1cf9b10b4a0f4f3db
lixixi89055465 <nanji1850094299@outlook.com>
2022-10-22 10:19:53 +0800

    * 886. Possible Bipartition

---
+++ src/zuo/intermediate/class03/Problem04_PrintMatrixSpiralOrder.java
@@ -0,0 +1,48 @@
+package zuo.intermediate.class03;
+
+public class Problem04_PrintMatrixSpiralOrder {
+    public static void spiralOrderPrint(int[][] matrix) {
+        int tR = 0;
+        int tC = 0;
+        int dR = 0;
+        int dC = 0;
+        while (tR <= dR && tC <= dC) {
+            printEdge(matrix, tR++, tC++, dR--, dC--);
+        }
+    }
+
+    private static void printEdge(int[][] matrix, int a, int b, int c, int d) {
+        if (a == c) {
+            for (int i = b; i <= d; i++) {
+                System.out.println(matrix[a][i]);
+            }
+        } else if (b == d) {
+            for (int i = a; i <= c; i++) {
+                System.out.println(matrix[i][b]);
+            }
+        } else {
+            int curC = b;
+            int curR = a;
+            while (curC != d) {
+                System.out.println(matrix[a][curC] + " ");
+                curC++;
+            }
+            while (curR != c) {
+                System.out.println(matrix[curR][d] + " ");
+                curR++;
+            }
+            while (curC != b) {
+                System.out.println(matrix[c][curC] + " ");
+                curC--;
+            }
+            while (curR != a) {
+                System.out.println(matrix[curR][b] + " ");
+                curR--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
