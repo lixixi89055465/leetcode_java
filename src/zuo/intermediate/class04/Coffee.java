bbf890fb4de6d5d06db3b7618f55f78c4082a6d1
lixixi89055465 <nanji1850094299@outlook.com>
2022-10-23 16:25:08 +0800

    zuo

---
+++ src/zuo/intermediate/class04/Coffee.java
@@ -0,0 +1,84 @@
+package zuo.intermediate.class04;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 喝咖啡
+ */
+public class Coffee {
+    public static int minTime1(int[] arr, int n, int a, int b) {
+        int[] times = new int[arr.length];
+        int[] drink = new int[n];
+        return forceMake(arr, times, 0, drink, n, a, b);
+    }
+
+    private static int forceMake(int[] arr, int[] times, int kth, int[] drink, int n, int a, int b) {
+        if (kth == n) {
+            int[] drinkSorted = Arrays.copyOf(drink, kth);
+            Arrays.sort(drinkSorted);
+            return forceWash(drinkSorted, a, b, 0, 0, 0);
+        }
+        int time = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int work = arr[i];
+            int pre = times[i];
+            drink[kth] = pre + work;
+        }
+        return 0;
+    }
+
+    private static int forceWash(int[] drinkSorted, int a, int b, int i, int i1, int i2) {
+        return 0;
+    }
+
+    static class Machine {
+        public int timePoint;
+        public int workTime;
+
+        public Machine(int timePoint, int workTime) {
+            this.timePoint = timePoint;
+            this.workTime = workTime;
+        }
+    }
+
+    public static class MachineComparator implements Comparator<Machine> {
+
+        @Override
+        public int compare(Machine o1, Machine o2) {
+            return o1.timePoint + o2.workTime - o2.timePoint - o2.timePoint;
+        }
+    }
+
+    public static int minTime2(int[] arr, int n, int a, int b) {
+        PriorityQueue<Machine> heap = new PriorityQueue<>(new MachineComparator());
+        for (int i = 0; i < arr.length; i++) {
+            heap.add(new Machine(0, arr[i]));
+        }
+        int[] drinks = new int[n];
+        for (int i = 0; i < n; i++) {
+            Machine cur = heap.poll();
+            cur.timePoint += cur.workTime;
+            drinks[i] = cur.timePoint;
+            heap.add(cur);
+        }
+        return process(drinks, a, b, 0, 0);
+    }
+
+    public static int process(int[] drinks, int a, int b, int index, int washLine) {
+        if (index == drinks.length - 1) {
+            return Math.min(Math.max(washLine, drinks[index]) + a, drinks[index] + b);
+        }
+        //当前决定 洗杯子
+        int wash = Math.max(washLine, drinks[index]) + a;
+        int next1 = process(drinks, a, b, index + 1, wash);
+        int p1 = Math.max(wash, next1);
+
+        //当前决定 挥发
+        int dry = drinks[index] + b;
+        int next2 = process(drinks, a, b, index + 1, washLine);
+        int p2 = Math.max(dry, next2);
+        return Math.min(p1, p2);
+    }
+}
