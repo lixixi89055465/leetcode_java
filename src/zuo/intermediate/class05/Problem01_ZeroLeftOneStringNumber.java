afcf530b26a41d2624eef1cad6bdf95b8e73b1ef
lixixi89055465 <nanji1850094299@outlook.com>
2022-10-24 00:03:41 +0800

    zuo

---
+++ src/zuo/intermediate/class05/Problem01_ZeroLeftOneStringNumber.java
@@ -0,0 +1,46 @@
+package zuo.intermediate.class05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem01_ZeroLeftOneStringNumber {
+    public static int fi(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[][] base = {{1, 1}, {1, 0}};
+        int[][] res = matrixPower(base, n - 2);
+        return res[0][0] + res[1][0];
+    }
+
+    private static int[][] matrixPower(int[][] m, int p) {
+        int[][] res = new int[m.length][m[0].length];
+        for (int i = 0; i < res.length; i++) {
+            res[i][i] = 1;
+        }
+        int[][] t = m;
+        for (; p != 0; p >>= 1) {
+            if ((p & 1) != 0) {
+                res = muliMatrix(res, t);
+            }
+            t = muliMatrix(t, t);
+        }
+        return res;
+    }
+
+    private static int[][] muliMatrix(int[][] m1, int[][] m2) {
+        int[][] res = new int[m1.length][m2.length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m2[0].length; j++) {
+                for (int k = 0; k < m2.length; k++) {
+                    res[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return res;
+    }
+
+}
