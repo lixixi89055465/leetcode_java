0d9c810ffaace55dca759718a065cad2dc82d4e6
lixixi89055465 <nanji1850094299@outlook.com>
2022-10-25 19:39:19 +0800

    zuo

---
+++ src/zuo/intermediate/class06/Problem02_BSTtoDoubleLinkedList.java
@@ -0,0 +1,16 @@
+package zuo.intermediate.class06;
+
+import java.util.TreeMap;
+
+public class Problem02_BSTtoDoubleLinkedList {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
