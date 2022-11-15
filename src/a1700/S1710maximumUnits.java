b1f5886a7c29aa0da408ae412f5dff8b159af94f
lixixi89055465 <1850094299@qq.com>
2022-11-15 14:27:48 +0800

    leetcode

---
+++ src/a1700/S1710maximumUnits.java
@@ -0,0 +1,28 @@
+package a1700;
+
+import java.util.Arrays;
+
+public class S1710maximumUnits {
+    protected static class Solution {
+        public int maximumUnits(int[][] boxTypes, int truckSize) {
+            Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+            int res = 0;
+            for (int[] boxType : boxTypes) {
+                int numberOfBoxes = boxType[0];
+                int numberOfUnitPerBox = boxType[1];
+                if (numberOfBoxes < truckSize) {
+                    res += numberOfBoxes * numberOfUnitPerBox;
+                    truckSize -= numberOfBoxes;
+                } else {
+                    res += truckSize * numberOfUnitPerBox;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
