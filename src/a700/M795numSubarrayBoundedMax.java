c4b30395382eac646d04c5b0aaf7d9ef827e4967
lixixi89055465 <nanji1850094299@outlook.com>
2022-11-24 16:14:49 +0800

    leetcode

---
+++ src/a700/M795numSubarrayBoundedMax.java
@@ -0,0 +1,13 @@
+package a700;
+
+public class M795numSubarrayBoundedMax {
+    protected static class Solution {
+        public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
