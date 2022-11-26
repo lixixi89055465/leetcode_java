195ad678ae20a1d4ce9691ab86e182f547633010
lixixi89055465 <nanji1850094299@outlook.com>
2022-11-26 19:54:22 +0800

    leetcode

--- src/zuo/base/chass01/Code02_BubbleSort.java
+++ src/zuo/base/chass01/Code02_BubbleSort.java
@@ -5,7 +5,7 @@ public class Code02_BubbleSort {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = arr.length - 1; i >= 0; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
