e0d51dc200d47f6aea73538c47555a1fd8a9a788
nanji <1850094299@qq.com>
2022-12-07 11:24:34 +0800

    leetcode

--- src/dp/ZeroOnePack.java
+++ src/dp/ZeroOnePack.java
@@ -1,10 +1,12 @@
 package dp;
 
+import java.util.Arrays;
+
 public class ZeroOnePack {
     public static int zeroOne(int N, int M, int[] w, int[] v) {
         int[][] dp = new int[N + 1][M + 1];
         for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; i < M + 1; i++) {
+            for (int j = 1; j < M + 1; j++) {
                 if (w[i - 1] > j) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
@@ -18,52 +20,90 @@ public class ZeroOnePack {
     public static int zeroOne1(int N, int M, int[] w, int[] v) {
         int[] dp = new int[M + 1];
         for (int i = 1; i < N + 1; i++) {
-            for (int j = M; i >= 0; j--) {
+            for (int j = M; j >= 0; j--) {
                 if (w[i - 1] <= j) {
                     dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
                 }
             }
+
         }
         return dp[M];
     }
 
     public static int zeroOne2(int N, int M, int[] w, int[] v) {
         int[] dp = new int[M + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
         dp[0] = 0;
-        for (int i = 1; i < M + 1; i++) {
-            dp[i] = Integer.MIN_VALUE;
-        }
         for (int i = 1; i < N + 1; i++) {
-            for (int j = M; j >= 0; j--) {
+            for (int j = M; j > 0; j--) {
                 if (w[i - 1] <= j) {
-                    dp[j] = Math.max(dp[j], v[i - 1] + dp[j - w[i - 1]]);
+                    dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
                 }
             }
         }
+        if (dp[M] < 0) dp[M] = 0;
         return dp[M];
     }
 
     public static int zeroOne3(int N, int M, int[] w, int[] v) {
         int[][] dp = new int[N + 1][M + 1];
-        int[][] path = new int[N + 1][M + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (w[i - 1] > j) {
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; i < M + 1; i++) {
+                if (w[i - 1] > M) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i][j], v[i - 1] + dp[i - 1][j - w[i - 1]]);
-                    path[i][j] = 1;
+                    int temp = Integer.MIN_VALUE;
+                    for (int k = 1; k <= j / w[i - 1]; k++) {
+                        temp = Math.max(temp, dp[i - 1][j - k * w[i - 1]] + k * v[i - 1]);
+                    }
+                    dp[i][j] = Math.max(temp, dp[i - 1][j]);
                 }
-            }
-        }
-        for (int i = N, j = M; i >= 0; i--) {
-            if (path[i][j] == 1) {
-                System.out.println("走了" + i + "," + j);
-                j -= w[i - 1];
             }
         }
         return dp[N][M];
     }
+
+    public static int zeroOne4(int N, int M, int[] w, int[] v) {
+        int[][] dp = new int[N + 1][M + 1];
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < M + 1; i++) {
+                if (w[i - 1] > M) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - w[i - 1]] + v[i - 1]);
+                }
+            }
+        }
+        return dp[N][M];
+    }
+
+    public static int zeroOne5(int N, int M, int[] w, int[] v) {
+        int[] dp = new int[M + 1];
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; i < M + 1; i++) {
+                if (w[i - 1] <= j) {
+                    dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
+                }
+            }
+
+        }
+        return dp[M];
+    }
+
+    public static int zeroOne6(int N, int M, int[] w, int[] v) {
+        int[] dp = new int[M + 1];
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < M + 1; j--) {
+                if (w[i - 1] <= j) {
+                    dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
+                }
+            }
+        }
+        if (dp[M] < 0) dp[M] = 0;
+        return dp[M];
+
+    }
+
 
     public static void main(String[] args) {
 //        int[] w = {2, 1, 3, 2};         // 物品的重量
