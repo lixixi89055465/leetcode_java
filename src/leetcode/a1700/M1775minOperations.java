fdac3a3e8571f02861c2ca80a7ba8ba6743f14fc
lixixi89055465 <nanji1850094299@outlook.com>
2022-12-08 15:46:03 +0800

    zuo

---
+++ src/leetcode/a1700/M1775minOperations.java
@@ -0,0 +1,9 @@
+package leetcode.a1700;
+
+public class M1775minOperations {
+    protected static class Solution {
+        public int minOperations(int[] nums1, int[] nums2) {
+            return 0;
+        }
+    }
+}
