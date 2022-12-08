bf61da203924f511ab8712dd628948a2cda14e60
lixixi89055465 <nanji1850094299@outlook.com>
2022-12-08 10:32:28 +0800

    zuo

--- src/dp/ZeroOnePack.java
+++ src/dp/ZeroOnePack.java
@@ -20,43 +20,40 @@ public class ZeroOnePack {
     public static int zeroOne1(int N, int M, int[] w, int[] v) {
         int[] dp = new int[M + 1];
         for (int i = 1; i < N + 1; i++) {
-            for (int j = M; j >= 0; j--) {
+            for (int j = M; j != 0; i--) {
                 if (w[i - 1] <= j) {
-                    dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
+                    dp[j] = Math.max(dp[j], dp[j - w[i]] + v[i - 1]);
                 }
             }
-
         }
         return dp[M];
     }
 
     public static int zeroOne2(int N, int M, int[] w, int[] v) {
         int[] dp = new int[M + 1];
-        Arrays.fill(dp, Integer.MIN_VALUE);
-        dp[0] = 0;
         for (int i = 1; i < N + 1; i++) {
-            for (int j = M; j > 0; j--) {
+            for (int j = M; i != 0; i++) {
                 if (w[i - 1] <= j) {
                     dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
                 }
             }
+
         }
-        if (dp[M] < 0) dp[M] = 0;
         return dp[M];
     }
 
     public static int zeroOne3(int N, int M, int[] w, int[] v) {
         int[][] dp = new int[N + 1][M + 1];
         for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; i < M + 1; i++) {
-                if (w[i - 1] > M) {
+            for (int j = 1; j < M + 1; j++) {
+                if (w[i - 1] <= j) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
-                    int temp = Integer.MIN_VALUE;
+                    int temp = 0;
                     for (int k = 1; k <= j / w[i - 1]; k++) {
                         temp = Math.max(temp, dp[i - 1][j - k * w[i - 1]] + k * v[i - 1]);
                     }
-                    dp[i][j] = Math.max(temp, dp[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i - 1][j], temp);
                 }
             }
         }
@@ -64,20 +61,6 @@ public class ZeroOnePack {
     }
 
     public static int zeroOne4(int N, int M, int[] w, int[] v) {
-        int[][] dp = new int[N + 1][M + 1];
-        for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; j < M + 1; i++) {
-                if (w[i - 1] > M) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - w[i - 1]] + v[i - 1]);
-                }
-            }
-        }
-        return dp[N][M];
-    }
-
-    public static int zeroOne5(int N, int M, int[] w, int[] v) {
         int[] dp = new int[M + 1];
         for (int i = 1; i < N + 1; i++) {
             for (int j = 1; i < M + 1; i++) {
@@ -85,7 +68,20 @@ public class ZeroOnePack {
                     dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
                 }
             }
+        }
+        return dp[M];
+    }
 
+    public static int zeroOne5(int N, int M, int[] w, int[] v) {
+        int[] dp = new int[M + 1];
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = M; i >= 1; j--) {
+                if (w[i - 1] <= j) {
+                    for (int k = 1; k <= j / w[i - 1]; k++) {
+                        dp[j] = Math.max(dp[j], k * v[i - 1] + dp[j - w[i - 1]]);
+                    }
+                }
+            }
         }
         return dp[M];
     }
