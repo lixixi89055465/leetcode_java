71c0a4bcc608c4daccca23a89ceddffcfd36c099
lixixi89055465 <nanji1850094299@outlook.com>
2023-03-24 10:58:35 +0800

    add pom.xml

---
+++ src/zuo/base/chass01/Test01.java
@@ -0,0 +1,15 @@
+package zuo.base.chass01;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int N=1000;
+        int a=0;
+        for (int i = 0; i < N; i++) {
+            a=2+7;
+            a=4*7;
+            a=6*8;
+        }
+        System.out.println(a);
+
+    }
+}
