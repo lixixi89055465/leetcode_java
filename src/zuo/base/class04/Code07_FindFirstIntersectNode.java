7e2d3cfcbe9392e22f290db0e8ab4009191aacac
lixixi89055465 <nanji1850094299@outlook.com>
2023-03-27 21:40:51 +0800

    leetcode

---
+++ src/zuo/base/class04/Code07_FindFirstIntersectNode.java
@@ -0,0 +1,32 @@
+package zuo.base.class04;
+
+public class Code07_FindFirstIntersectNode {
+    private static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node n1 = head.next, n2 = head.next.next;
+        while (n1 != n2) {
+            if (n2.next == null || n2.next.next == null) {
+                return null;
+            }
+            n2 = n2.next.next;
+            n1 = n1.next;
+        }
+        n2 = head;
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1;
+    }
+}
