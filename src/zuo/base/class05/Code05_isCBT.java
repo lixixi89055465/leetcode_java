b8ff12117dbdad0732c7c95751c921df51a6d1b0
lixixi89055465 <nanji1850094299@outlook.com>
2023-03-28 11:37:25 +0800

    zuo-base

---
+++ src/zuo/base/class05/Code05_isCBT.java
@@ -0,0 +1,23 @@
+package zuo.base.class05;
+
+import java.util.LinkedList;
+
+public class Code05_isCBT {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+    }
+
+    public static boolean isCBT(Node head) {
+        if (head == null) {
+            return true;
+        }
+        LinkedList<Object> queue = new LinkedList<>();
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
