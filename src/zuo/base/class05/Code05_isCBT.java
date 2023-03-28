d1b6976fbd55f9cbfc1dd48f7dddf573d040ed67
lixixi89055465 <nanji1850094299@outlook.com>
2023-03-28 14:36:30 +0800

    zuo-base

--- src/zuo/base/class05/Code05_isCBT.java
+++ src/zuo/base/class05/Code05_isCBT.java
@@ -1,6 +1,7 @@
 package zuo.base.class05;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Code05_isCBT {
     public static class Node {
@@ -10,10 +11,27 @@ public class Code05_isCBT {
     }
 
     public static boolean isCBT(Node head) {
-        if (head == null) {
-            return true;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        Node l = null, r = null;
+        boolean leaf = false;
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            l = head.left;
+            r = head.right;
+            if ((leaf && (l != null || r != null)) || (l == null && r != null)) {
+                return false;
+            }
+            if (l != null) {
+                queue.add(l);
+            }
+            if (r != null) {
+                queue.add(r);
+            }
+            if (l == null || r == null) {
+                leaf = true;
+            }
         }
-        LinkedList<Object> queue = new LinkedList<>();
         return true;
     }
 
