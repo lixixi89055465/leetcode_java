be4ea9b357f3698f27cef1026f0fe05b13ad35af
lixixi89055465 <1850094299@qq.com>
2023-03-29 12:08:32 +0800

    zuo leetcode

---
+++ src/zuo/base/class06/Graph.java
@@ -0,0 +1,14 @@
+package zuo.base.class06;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Graph {
+    public HashMap<Integer, Node> nodes;
+    public HashSet<Edge> edges;
+
+    public Graph() {
+        this.nodes = new HashMap<>();
+        this.edges = new HashSet<>();
+    }
+}
