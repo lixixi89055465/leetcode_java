4762ddb33451f99a543d2b902132136215ba6df3
lixixi89055465 <1850094299@qq.com>
2023-04-21 23:15:19 +0800

    zuo leetcode

---
+++ src/zuo/base/class05/Code02_MaxDistanceInTree.java
@@ -0,0 +1,38 @@
+package zuo.base.class05;
+
+public class Code02_MaxDistanceInTree {
+    private static class Node1 {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node1(int value) {
+            this.value = value;
+        }
+    }
+
+    private static class Info {
+        public int maxDistance;
+        public int height;
+
+        public Info(int maxDistance, int height) {
+            this.maxDistance = maxDistance;
+            this.height = height;
+        }
+    }
+
+    public static Info process(Node x) {
+        if (x == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        //info
+        int p1 = leftInfo.maxDistance;
+        int p2 = rightInfo.maxDistance;
+        int p3 = leftInfo.height + 1 + rightInfo.height;
+        int maxDistance = Math.max(p3, Math.max(p1, p2));
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        return new Info(maxDistance, height);
+    }
+}
