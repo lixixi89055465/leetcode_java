6ff22200a08e8222dbb4e645d54f893bd3197ec2
lixixi89055465 <1850094299@qq.com>
2023-04-25 12:26:39 +0800

    zuo leetcode

--- src/zuo/base/class06/Code02_Power.java
+++ src/zuo/base/class06/Code02_Power.java
@@ -11,6 +11,13 @@ public class Code02_Power {
     }
 
     public static boolean is4Power(int n) {
-        return (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+        return is2Power(n) && (n & 0x55555555) != 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        int a = n - 1;
+        int b = n & a;
+        System.out.println(b);
     }
 }
