cdf111bb6d1fa22dbff81417044e25846259773c
lixixi89055465 <1850094299@qq.com>
2023-05-16 21:50:24 +0800

    zuo leetcode

--- src/zuo/intermediate/class05/Test03.java
+++ src/zuo/intermediate/class05/Test03.java
@@ -4,6 +4,7 @@ public class Test03 {
     public static void main(String[] args) {
         System.out.println(Integer.MIN_VALUE);
         System.out.println(Integer.MIN_VALUE / 10);
+        System.out.println(Integer.MIN_VALUE % 10);
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Integer.MAX_VALUE / 10);
     }
