b3ab23d5978d02370bd5150b2397bfee96f36eeb
lixixi89055465 <1850094299@qq.com>
2023-05-16 11:32:24 +0800

    zuo leetcode

---
+++ src/zuo/intermediate/class05/test02.java
@@ -0,0 +1,30 @@
+package zuo.intermediate.class05;
+
+public class test02 {
+    public static int[][] matrixPower(int[][] m, int p) {
+        int[][] res = new int[m.length][m[0].length];
+        for (int i = 0; i < res.length; i++) {
+            res[i][i] = 1;
+        }
+        int[][] t = m;
+        for (; p != 0; p >>= 1) {
+            if ((p & 1) != 0) {
+                res = muliMatrix(res, t);
+            }
+            t = muliMatrix(t, t);
+        }
+        return res;
+    }
+
+    public static int[][] muliMatrix(int[][] m1, int[][] m2) {
+        int[][] res = new int[m1.length][m1[0].length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m2[0].length; j++) {
+                for (int k = 0; k < m1[0].length; k++) {
+                    res[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return res;
+    }
+}
