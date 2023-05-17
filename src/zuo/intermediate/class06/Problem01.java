16de746689cfd172608e20bc804cb2893427b021
nanji <1850094299@qq.com>
2023-05-17 15:50:23 +0800

    zuo

--- src/zuo/intermediate/class06/Problem01.java
+++ src/zuo/intermediate/class06/Problem01.java
@@ -12,5 +12,59 @@ public class Problem01 {
             this.nextMap = new TreeMap<>();
         }
     }
+
+    public static void print(String[] folderpaths) {
+        if (folderpaths == null || folderpaths.length == 0) {
+            return;
+        }
+        Node head = generateFolder(folderpaths);
+        printProcess(head, 0);
+    }
+
+    private static void printProcess(Node node, int level) {
+        if (level != 0) {
+            System.out.println(get2nSpace(level) + node.name);
+        }
+        for (Node next : node.nextMap.values()) {
+            printProcess(next, level + 1);
+        }
+    }
+
+    private static String get2nSpace(int level) {
+        String res = "";
+        for (int i = 1; i < level; i++) {
+            res += ",";
+        }
+        return res;
+    }
+
+    private static Node generateFolder(String[] folderpaths) {
+        Node head = new Node("");
+        for (int i = 0; i < folderpaths.length; i++) {
+            Node cur = head;
+            String[] paths = folderpaths[i].split("\\\\");
+            for (int i1 = 0; i1 < paths.length; i1++) {
+                if (!cur.nextMap.containsKey(paths[i1])) {
+                    cur.nextMap.put(paths[i1], new Node(paths[i1]));
+                }
+                cur = cur.nextMap.get(paths[i1]);
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        String[] folders = {
+                "a\\b\\c",
+                "d\\f\\g",
+                "e\\i\\h",
+                "e\\i\\j",
+                "e\\i\\k",
+                "e\\i\\k",
+        };
+        print(folders);
+    }
+
+
 }
 
--- src/zuo/intermediate/class06/Problem02_BSTtoDoubleLinkedList.java
+++ src/zuo/intermediate/class06/Problem02_BSTtoDoubleLinkedList.java
@@ -13,4 +13,44 @@ public class Problem02_BSTtoDoubleLinkedList {
             this.value = value;
         }
     }
+
+    private static class Info {
+        public Node start;
+        public Node end;
+
+        public Info(Node start, Node end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static Info process(Node X) {
+        if (X == null) {
+            return new Info(null, null);
+        }
+        Info leftHeadEnd = process(X.left);
+        Info rightHeadEnd = process(X.right);
+        if (leftHeadEnd.end != null) {
+            leftHeadEnd.end.right = X;
+        }
+        X.left = leftHeadEnd.end;
+        X.right = rightHeadEnd.start;
+        if (rightHeadEnd.start != null) {
+            rightHeadEnd.start.left = X;
+        }
+        return new Info(leftHeadEnd.start != null ? leftHeadEnd.start : X,
+                rightHeadEnd.end != null ? X : rightHeadEnd.end);
+    }
+
+    public static void printBSTInOrder(Node head) {
+        System.out.println("BST in - order :");
+        if (head != null) {
+            inOrderPrint(head);
+
+        }
+    }
+
+    private static void inOrderPrint(Node head) {
+
+    }
 }
