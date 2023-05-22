a95f308660c60a44e755d3e9d5f88c6799e35ea3
lixixi89055465 <nanji1850094299@outlook.com>
2023-05-22 10:02:58 +0800

    zuo leetcode

--- src/zuo/intermediate/class08/Problem03.java
+++ src/zuo/intermediate/class08/Problem03.java
@@ -2,40 +2,37 @@ package zuo.intermediate.class08;
 
 public class Problem03 {
     public static int g(int i, int len) {
-        int result = 0;
-        for (int j = i + 1; j < 26 - len; j++) {
-            result += g(j, len - 1);
+        int sum = 0;
+        if (len == 1) {
+            return 1;
         }
-        return result;
+        for (int j = i + 1; j < 26; j++) {
+            sum += g(j, len - 1);
+        }
+        return sum;
     }
 
     public static int f(int len) {
-        int result = 0;
-        for (int i = 0; i < 26 - len; i++) {
-            result += g(i, len);
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum += g(i, len);
         }
-        return result;
+        return sum;
     }
 
     public static int kth(String s) {
         char[] chars = s.toCharArray();
         int sum = 0;
-        int len = s.length();
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < chars.length; i++) {
             sum += f(i);
         }
-        for (int i = 0; i < chars[0] - 'a' + 1; i++) {
-            sum += g(i, len);
-        }
-        int pre = chars[0] - 'a' + 1;
+        int pre = chars[0] - 'a';
         for (int i = 1; i < chars.length; i++) {
-            int cur = chars[i] - 'a' + 1;
-            for (int j = pre + 1; j < cur; j++) {
-                sum += g(j, len - i);
+            for (int j = pre + 1; j < chars[i] - 'a'; j++) {
+                sum += g(j, chars.length - i);
             }
-            pre = cur;
+            pre=chars[i]-'a';
         }
-        return sum + 1;
+        return sum;
     }
-
 }
