ed71d89d1f4b3af884a227235b39eb5162229e09
lixixi89055465 <1850094299@qq.com>
2023-05-23 20:57:47 +0800

    zuo leetcode

--- src/test1.java
+++ src/test1.java
@@ -4,53 +4,51 @@ import java.util.Scanner;
 
 
 public class test1 {
-    public static void main(String[] args) {
-        int[] arry = {1,9,5,2,4,8};
-        quickSort(arry, 0, arry.length - 1);
-        System.out.println(Arrays.toString(arry));
+    private static class ListNode {
+        int val;
+        ListNode next = null;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
     }
 
-    public static void quickSort(int[] arry, int left, int right) {
-        //运行判断，如果左边索引大于右边是不合法的，直接return结束次方法
-        if (left > right) {
-            return;
+    public ListNode partition(ListNode pHead, int x) {
+        if (pHead == null) {
+            return pHead;
         }
-        //定义变量保存基准数
-        int base = arry[left];
-        //定义变量i，指向最左边
-        int i = left;
-        //定义j ,指向最右边
-        int j = right;
-        //当i和j不相遇的时候，再循环中进行检索
-        while (i != j) {
-            //先由j从右往左检索比基准数小的，如果检索到比基准数小的就停下。
-            //如果检索到比基准数大的或者相等的就停下
-            while (arry[j] >= base && i < j) {
-                j--; //j从右往左检索
-
+        ListNode lowl = null, lowr = null, midl = null, midr = null, highl = null, highr = null;
+        ListNode head = pHead;
+        while (head != null) {
+            if (head.val > x) {
+                if (lowl == null) {
+                    lowl = head;
+                    lowr = head;
+                } else {
+                    lowr.next = head;
+                }
+            } else if (head.val == x) {
+                if (midl == null) {
+                    midl = head;
+                    midr = head;
+                } else {
+                    midr.next = head;
+                }
+            } else {
+                if (highl == null) {
+                    highl = head;
+                    highr = head;
+                } else {
+                    highr.next = head;
+                }
             }
-            while (arry[i] <= base && i < j) {
-                i++; //i从左往右检索
-            }
-            //代码走到这里i停下，j也停下，然后交换i和j位置的元素
-            int tem = arry[i];
-            arry[i] = arry[j];
-            arry[j] = tem;
-
-
         }
-        //如果上面while条件不成立就会跳出这个循环，往下执行
-        //如果这个条件不成立就说明 i和j相遇了
-        //如果i和j相遇了，就交换基准数这个元素和相遇位置的元素
-        //把相遇元素的值赋给基准数这个位置的元素
-        arry[left] = arry[i];
-        //把基准数赋给相遇位置的元素
-        arry[i] = base;
-        //基准数在这里递归就为了左边的数比它小，右边的数比它大
-        //排序基准数的左边
-        quickSort(arry, left, i - 1);
-        //排右边
-        quickSort(arry, j + 1, right);
+        lowr.next=midl;
+        midr.next=midl;
+        return pHead;
+    }
+
+    public static void main(String[] args) {
 
     }
 }
--- src/zuo/intermediate/class08/Problem05_EditCost.java
+++ src/zuo/intermediate/class08/Problem05_EditCost.java
@@ -3,22 +3,22 @@ package zuo.intermediate.class08;
 public class Problem05_EditCost {
 
     public static int minCost1(String str1, String str2, int dc, int ic, int rc) {
-        if (str1 == null || str2 == null) {
+        if (str1 == null || str2 == null||str1.length()==0||str2.length()==0) {
             return 0;
         }
         char[] chs1 = str1.toCharArray();
         char[] chs2 = str2.toCharArray();
-        int row = chs1.length - 1;
-        int col = chs2.length - 1;
+        int row = chs1.length +1;
+        int col = chs2.length +1;
         int[][] dp = new int[row][col];
-        for (int i = 0; i < row; i++) {
+        for (int i = 0; i <row; i++) {
             dp[i][0] = dc * i;
         }
         for (int i = 0; i < col; i++) {
             dp[0][i] = ic * i;
         }
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
+        for (int i = 1; i <row; i++) {
+            for (int j = 1; j <col; j++) {
                 if (chs1[i - 1] == chs2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
@@ -31,7 +31,14 @@ public class Problem05_EditCost {
         return dp[row - 1][col - 1];
     }
 
-    public static void main(String[] args) {
+    public static  int minOperationCount(String source, String target) {
+        // write code here
+        int result = minCost1(source, target, 1, 1, 1);
+        return result;
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minOperationCount("abc", "de"));
     }
 }
