13b78b130e88b2eee60efa75b3d7043485633c91
lixixi89055465 <nanji1850094299@outlook.com>
2023-05-26 15:03:42 +0800

    zuo leetcode

---
+++ src/zuo/senior/class02/Problem02_LongestSumSubArrayLengthInPositiveArray.java
@@ -0,0 +1,33 @@
+package zuo.senior.class02;
+
+public class Problem02_LongestSumSubArrayLengthInPositiveArray {
+
+    public static int getMaxLength(int[] arr, int k) {
+        if (arr == null || arr.length == 0 || k <= 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int sum = arr[0];
+        int len = 0;
+        while (right < arr.length) {
+            if (sum == k) {
+                len = Math.max(len, right - left + 1);
+                sum -= arr[left++];
+            } else if (sum < k) {
+                right++;
+                if (right == arr.length) {
+                    break;
+                }
+                sum += arr[right];
+            } else {
+                sum -= arr[left++];
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
