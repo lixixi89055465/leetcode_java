ca4414fa83a0fd5370c5d9ea280cd08b066f0f96
lixixi89055465 <1850094299@qq.com>
2023-06-04 15:28:49 +0800

    zuo leetcode

--- src/zuo/senior/Problem02_SplitNumer.java
+++ src/zuo/senior/Problem02_SplitNumer.java
@@ -24,8 +24,22 @@ public class Problem02_SplitNumer {
         for (int pre = n; pre > 0; pre--) {
             for (int rest = pre; rest <= n; rest++) {
                 for (int i = pre; i <= rest; i++) {
-                    dp[pre][rest] += dp[i][rest-i];
+                    dp[pre][rest] += dp[i][rest - i];
                 }
+            }
+        }
+        return dp[1][n];
+    }
+
+    private static int process3(int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+        for (int pre = n; pre > 0; pre--) {
+            for (int rest = pre; rest <= n; rest++) {
+                dp[pre][rest] += dp[pre][rest - pre] + dp[pre + 1][rest];
             }
         }
         return dp[1][n];
