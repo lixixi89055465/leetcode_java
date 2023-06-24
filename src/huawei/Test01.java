b34821b09610351e8deb7f10740cc3aaa4a56ece
lixixi89055465 <nanji1850094299@outlook.com>
2023-06-24 18:27:22 +0800

    zuo leetcode

--- src/huawei/Test01.java
+++ src/huawei/Test01.java
@@ -2,10 +2,9 @@ package huawei;
 
 public class Test01 {
     public static void main(String[] args) {
-        String str = "0.2.3.8";
-        String[] split = str.split("\\.");
-        for (int i = 0; i < 4; i++) {
-            System.out.println(split[i]);
-        }
+        char c='a';
+        char b='1';
+        System.out.println(Character.isLetter(c));
+        System.out.println(Character.isLetter(b));
     }
 }
