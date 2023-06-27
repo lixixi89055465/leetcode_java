83f30bc96239f18fe88241dedce2f9891cca1ad0
lixixi89055465 <nanji1850094299@outlook.com>
2023-06-27 15:26:43 +0800

    zuo leetcode

--- src/huawei/Test01.java
+++ src/huawei/Test01.java
@@ -2,9 +2,9 @@ package huawei;
 
 public class Test01 {
     public static void main(String[] args) {
-        char c='a';
-        char b='1';
-        System.out.println(Character.isLetter(c));
-        System.out.println(Character.isLetter(b));
+        int a = 1;
+        while (a == 1) {
+            System.out.println(a);
+        }
     }
 }
--- src/zuo/publicclass2020/class002/Code02_KthMinPair.java
+++ src/zuo/publicclass2020/class002/Code02_KthMinPair.java
@@ -161,7 +161,7 @@ public class Code02_KthMinPair {
 		}
 	}
 
-	public static void swap(int[] arr, int index1, int index2) {
+	private static void swap(int[] arr, int index1, int index2) {
 		int tmp = arr[index1];
 		arr[index1] = arr[index2];
 		arr[index2] = tmp;
