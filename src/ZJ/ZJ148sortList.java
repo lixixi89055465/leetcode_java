5eacea1186c4a4e8e817bc91ed565c67af559f5e
lixixi89055465 <1850094299@qq.com>
2023-07-11 18:34:30 +0800

    ZJ leetcode

---
+++ src/ZJ/ZJ148sortList.java
@@ -0,0 +1,62 @@
+package ZJ;
+
+/**
+ * 148. 排序链表
+ * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [4,2,1,3]
+ * 输出：[1,2,3,4]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [-1,5,3,4,0]
+ * 输出：[-1,0,3,4,5]
+ * 示例 3：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目在范围 [0, 5 * 104] 内
+ * -105 <= Node.val <= 105
+ * <p>
+ * <p>
+ * 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ */
+public class ZJ148sortList {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static class Solution {
+        public ListNode sortList(ListNode head) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solve = new Solution();
+        ListNode res = solve.sortList(null);
+        System.out.println(res);
+    }
+}
