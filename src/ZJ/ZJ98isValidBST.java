56ba38579a2606508177821c2036ca66538460b5
lixixi89055465 <nanji1850094299@outlook.com>
2023-07-22 12:44:39 +0800

    zj leetcode

--- src/ZJ/ZJ98isValidBST.java
+++ src/ZJ/ZJ98isValidBST.java
@@ -25,23 +25,24 @@ public class ZJ98isValidBST {
         }
     }
 
-    private static class Node {
-        private Integer minV;
-        private Integer maxV;
-        private boolean isValid;
-
-        public Node(Integer minV, Integer maxV, boolean isValid) {
-            this.minV = minV;
-            this.maxV = maxV;
-            this.isValid = isValid;
-        }
-
-        public Node(boolean isValid) {
-            this.isValid = isValid;
-        }
-    }
-
     private static class Solution {
+        private static class Node {
+            private Integer minV;
+            private Integer maxV;
+            private boolean isValid;
+
+            public Node(Integer minV, Integer maxV, boolean isValid) {
+                this.minV = minV;
+                this.maxV = maxV;
+                this.isValid = isValid;
+            }
+
+            public Node(boolean isValid) {
+                this.isValid = isValid;
+            }
+        }
+
+
         public boolean isValidBST(TreeNode root) {
             return root != null ? check(root).isValid : true;
         }
@@ -54,14 +55,14 @@ public class ZJ98isValidBST {
             Node right = check(root.right);
             Integer minV = root.val;
             if (left != null) {
-                if (!left.isValid || left.maxV < root.val) {
+                if (!left.isValid || left.maxV >=root.val) {
                     return new Node(false);
                 }
                 minV = left.minV;
             }
             Integer maxV = root.val;
             if (right != null) {
-                if (!right.isValid || right.minV < root.val) {
+                if (!right.isValid || right.minV <= root.val) {
                     return new Node(false);
                 }
                 maxV = right.maxV;
