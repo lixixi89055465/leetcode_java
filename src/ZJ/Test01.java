bcfc10fa43ee70b9f02117c0dfab6189fe997234
lixixi89055465 <1850094299@qq.com>
2023-08-08 14:20:45 +0800

    ZJ leetcode

---
+++ src/ZJ/Test01.java
@@ -0,0 +1,10 @@
+package ZJ;
+
+public class Test01 {
+    public static void main(String[] args) {
+        char c1='2';
+        char c2='4';
+        int a1=c1-'0';
+        System.out.println(a1);
+    }
+}
