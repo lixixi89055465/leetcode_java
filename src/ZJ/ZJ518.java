733294aff8fccc0aa37baba75f5a3e2dbf4c7ac6
lixixi89055465 <1850094299@qq.com>
2023-08-10 19:33:05 +0800

    ZJ leetcode

---
+++ src/ZJ/ZJ518.java
@@ -0,0 +1,12 @@
+package ZJ;
+
+/**
+ *
+ */
+public class ZJ518 {
+    private static class Solution {
+        public int minFallingPathSum(int[][] grid) {
+            return 0;
+        }
+    }
+}
