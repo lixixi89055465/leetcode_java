aebcc8e37e645c906468f7fa80aea289fcad1015
lixixi89055465 <1850094299@qq.com>
2023-08-17 19:02:24 +0800

    ZJ leetcode

--- src/leetcode/mst/H0813pileBox.java
+++ src/leetcode/mst/H0813pileBox.java
@@ -1,5 +1,7 @@
 package leetcode.mst;
 
+import java.util.*;
+
 /**
  * 面试题 08.13. 堆箱子
  * 提示
@@ -24,12 +26,53 @@ package leetcode.mst;
  */
 public class H0813pileBox {
     private static class Solution {
+
         public int pileBox(int[][] box) {
-            return 0;
+            int n = box.length;
+            HashMap<Integer, List<Integer>> map = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (box[i][0] < box[j][0] && box[i][1] < box[j][1] && box[i][2] < box[j][2]) {
+                        List<Integer> jNext = map.getOrDefault(j, new ArrayList<Integer>());
+                        jNext.add(i);
+                        map.put(j, jNext);
+                    }
+                    if (box[i][0] > box[j][0] && box[i][1] > box[j][1] && box[i][2] > box[j][2]) {
+                        List<Integer> iNext = map.getOrDefault(i, new ArrayList<Integer>());
+                        iNext.add(j);
+                        map.put(i, iNext);
+                    }
+                }
+            }
+            int res = 0;
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                res = Math.max(res, dfs(map, i,  box, dp));
+            }
+            return res;
+        }
+
+        private int dfs(HashMap<Integer, List<Integer>> map, int value, int[][] box, int[] dp) {
+            if (dp[value] > 0) {
+                return dp[value];
+            }
+//            stack += box[value][2];
+            int maxStack = 0;
+            if (map.containsKey(value) && map.get(value).size() > 0) {
+                for (Integer nextValue : map.get(value)) {
+                    maxStack = Math.max(maxStack, dfs(map, nextValue, box, dp));
+                }
+            }
+            dp[value] = maxStack + box[value][2];
+            return dp[value];
         }
     }
 
     public static void main(String[] args) {
-
+//        int[][] box = {{1, 1, 1}, {2, 3, 4}, {2, 6, 7}, {3, 4, 5}};
+//        int[][] box = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        int[][] box = {{9, 9, 10}, {8, 10, 9}, {9, 8, 10}, {9, 8, 10}, {10, 8, 8}, {9, 8, 9}, {9, 8, 8}, {8, 9, 10}, {10, 9, 10}, {8, 8, 10}, {10, 9, 10}, {10, 9, 8}, {8, 9, 9}, {9, 10, 8}, {8, 9, 9}, {10, 10, 9}, {8, 9, 10}, {8, 10, 10}, {8, 9, 10}, {10, 10, 8}, {10, 10, 9}, {9, 10, 10}, {10, 8, 9}, {10, 10, 9}, {8, 9, 10}, {8, 8, 9}, {8, 10, 10}, {9, 9, 10}, {10, 8, 8}, {10, 10, 8}, {8, 9, 9}, {8, 9, 8}, {10, 10, 8}, {8, 10, 8}, {10, 9, 10}, {9, 9, 10}, {9, 9, 9}, {8, 9, 8}, {9, 8, 8}, {8, 9, 10}, {10, 10, 8}, {9, 9, 9}, {10, 10, 10}, {10, 9, 8}, {9, 8, 9}, {8, 8, 10}, {8, 8, 8}, {8, 8, 8}, {8, 9, 10}, {10, 9, 8}, {8, 10, 8}, {8, 10, 10}, {9, 10, 10}, {8, 8, 9}, {9, 9, 9}, {10, 8, 8}, {8, 10, 10}, {9, 10, 9}, {9, 9, 8}, {8, 10, 9}, {9, 8, 8}, {9, 9, 10}, {9, 10, 10}, {8, 8, 10}};
+        Solution solve = new Solution();
+        System.out.println(solve.pileBox(box));
     }
 }
---
+++ src/leetcode/mst/H1603intersection.java
@@ -0,0 +1,55 @@
+package leetcode.mst;
+
+/**
+ * 面试题 16.03. 交点
+ * 提示
+ * 困难
+ * 84
+ * 相关企业
+ * 给定两条线段（表示为起点start = {X1, Y1}和终点end = {X2, Y2}），如果它们有交点，请计算其交点，没有交点则返回空值。
+ * <p>
+ * 要求浮点型误差不超过10^-6。若有多个交点（线段重叠）则返回 X 值最小的点，X 坐标相同则返回 Y 值最小的点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * line1 = {0, 0}, {1, 0}
+ * line2 = {1, 1}, {0, -1}
+ * 输出： {0.5, 0}
+ * 示例 2：
+ * <p>
+ * 输入：
+ * line1 = {0, 0}, {3, 3}
+ * line2 = {1, 1}, {2, 2}
+ * 输出： {1, 1}
+ * 示例 3：
+ * <p>
+ * 输入：
+ * line1 = {0, 0}, {1, 1}
+ * line2 = {1, 0}, {2, 1}
+ * 输出： {}，两条线段没有交点
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 坐标绝对值不会超过 2^7
+ * 输入的坐标均是有效的二维坐标
+ */
+public class H1603intersection {
+    private static class Solution {
+        public double[] intersection(int[] start1, int[] end1, int[] start2, int[] end2) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solve = new Solution();
+        int[] start1 = {0, 0};
+        int[] end1 = {1, 0};
+        int[] start2 = {1, 1};
+        int[] end2 = {0, -1};
+        System.out.println(solve.intersection(start1, end1, start2, end2));
+    }
+}
