f44491df715ad83bf3c3592edafc507f45ff5656
lixixi89055465 <1850094299@qq.com>
2023-08-31 20:30:20 +0800

    leetcode top100

--- src/leetcode/top100/H315countSmaller.java
+++ src/leetcode/top100/H315countSmaller.java
@@ -1,6 +1,6 @@
 package leetcode.top100;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @PackageName:leetcode.top100
@@ -11,8 +11,6 @@ import java.util.List;
  * 1K
  * 相关企业
  * 给你一个整数数组 nums ，按要求返回一个新数组 counts 。数组 counts 有该性质： counts[i] 的值是  nums[i] 右侧小于 nums[i] 的元素的数量。
- * <p>
- * <p>
  * <p>
  * 示例 1：
  * <p>
@@ -39,15 +37,61 @@ import java.util.List;
  * -104 <= nums[i] <= 104
  */
 public class H315countSmaller {
+
     private static class Solution {
+        private class BinaryTree {
+            private int[] tree;
+            private int maxLen;
+
+            public BinaryTree(int maxLen) {
+                this.maxLen = maxLen;
+                this.tree = new int[this.maxLen];
+            }
+
+            private void update(int i, int x) {
+                if (i > maxLen) {
+                    return;
+                }
+                for (int pos = i; pos <= this.maxLen; pos += lowBit(pos)) {
+                    tree[pos - 1] += x;
+                }
+            }
+
+            public int query(int end) {
+                if (end > this.maxLen || end <= 0) {
+                    return 0;
+                }
+                int ans = 0;
+                for (int pos = end; pos > 0; pos -= lowBit(pos)) {
+                    ans += tree[pos - 1];
+                }
+                return ans;
+            }
+            private int lowBit(int pos) {
+                return pos & (-pos);
+            }
+        }
+
+
         public List<Integer> countSmaller(int[] nums) {
-            return null;
+            int maxLen = 209;
+            BinaryTree btree = new BinaryTree(maxLen);
+            Integer[] res = new Integer[nums.length];
+            for (int i = nums.length - 1; i >= 0; i--) {
+                nums[i] += 104;
+                res[i] = btree.query(nums[i]-1);
+                btree.update(nums[i], 1);
+            }
+            return Arrays.asList(res);
         }
     }
 
     public static void main(String[] args) {
         Solution solve = new Solution();
-        int[] nums = {5, 2, 6, 1};
+//        int[] nums = {5, 2, 6, 1};
+//        int []nums = {-1};
+        int[] nums = {-1, -1};
         System.out.println(solve.countSmaller(nums));
+
     }
 }
