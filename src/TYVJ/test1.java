e19650c2f8aa0e2368363f0d86773a3ea2bb17e8
lixixi89055465 <wb_xiang.li4@sinitek.com>
2023-09-08 13:59:54 +0800

    leetcode 算法学习笔记

--- src/TYVJ/test1.java
+++ src/TYVJ/test1.java
@@ -2,16 +2,19 @@ package TYVJ;
 
 import org.omg.CORBA.INTERNAL;
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class test1 {
     public static void main(String[] args) {
-        int N=10;
+        int N = 10;
 //        int res=Floyd(N);
 //        System.out.println(res);
     }
-    public static int []Bellman_Ford(int n,int m){
-        int[]dist=new int[n];
+
+    public static int[] Bellman_Ford(int n, int m) {
+        int[] dist = new int[n];
 
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < m; j++) {
@@ -22,19 +25,20 @@ public class test1 {
     }
 
     private static int[][] Floyd(int N) {
-        int [][]dist=new int[N+1][N+1];
+        int[][] dist = new int[N + 1][N + 1];
 
         for (int k = 1; k <= N; k++) {
             for (int i = 1; i <= N; i++) {
                 for (int j = 1; j <= N; j++) {
-                    dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
         System.out.println(dist);
         return dist;
     }
-//    private static void SPFA(int S,int N){
+
+    //    private static void SPFA(int S,int N){
 //        int[]inQueue=new int[N];
 //        LinkedList<Integer>Q=new LinkedList<>();
 //        Q.addLast(S);
@@ -48,6 +52,38 @@ public class test1 {
 //                    if(!inqueue[to]){
 //                        inQueue[to]=1;
 //                        Q.push(to);
+//                    }
+//                }
+//            }
+//        }
+//    }
+    private class Node {
+        public int dist;
+        public int index;
+
+        public Node(int dist, int index) {
+            this.dist = dist;
+            this.index = index;
+        }
+    }
+
+//    private static void dij(int s,int N) {
+//        PriorityQueue<Node> minstack = new PriorityQueue<>(Comparator.comparingInt(o -> o.dist));
+//        minstack.add(new Node(s,0));
+//        int []pre=new int[N];
+//        while (!minstack.isEmpty()){
+//            Node p = minstack.poll();
+//            if(visit[p.index]){
+//                continue;
+//            }
+//            for (int e=head[p.index];e!=0;e=edges[e].next ){
+//                int to=edges[e].to;
+//                if(edges[e].w+dist[p]<dist[to]) {
+//                    pre[to] = p;
+//                    dist[to] = edges[e].w + dist[p];
+//                    if(!inqueue[to]){
+//                        minstack.push(to);
+//                        inqueue[to]=1;
 //                    }
 //                }
 //            }
