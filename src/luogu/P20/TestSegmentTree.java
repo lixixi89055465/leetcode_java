95997a34a8ba971d5d0e6a71c160171cd228f2c0
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-09-15 18:02:27 +0800

    leetcode 算法学习笔记

--- src/luogu/P20/TestSegmentTree.java
+++ src/luogu/P20/TestSegmentTree.java
@@ -6,6 +6,10 @@ package luogu.P20;
  * @Description:
  */
 public class TestSegmentTree {
+    public static void main(String[] args) {
+
+    }
+
     public void buildTree(int[] tree, int s, int e, int p, int[] A) {
         if (s == e) {
             tree[p] = A[s];
@@ -32,66 +36,38 @@ public class TestSegmentTree {
         return sum;
     }
 
-    int getSum(int[] tree, int L, int R, int S, int T, int P, int[] B) {
-        if (L <= S && T <= R) {
-            return tree[P];
-        }
-        int m = S + (S - T) >> 1;
-        if (B[P] > 0) {
-            tree[P * 2] += B[P] * (m - S + 1);
-            B[P * 2] += B[P];
-            tree[P * 2 + 1] += B[P] * (T - m);
-            B[P * 2 + 1] += B[P];
-            B[P] = 0;
-        }
-        int sum = 0;
-        if (L <= m) {
-            sum += getSum(tree, L, R, S, m, P * 2, B);
-        }
-        if (R >= m) {
-            sum += getSum(tree, L, R, m + 1, T, P * 2 + 1, B);
-        }
-        return sum;
+    int getSum01(int[] tree, int L, int R, int S, int T, int p, int[] B) {
+        return 0;
     }
 
     public void update(int[] tree, int L, int R, int c, int S, int T, int P, int[] B) {
-        if (L <= S && T <= R) {
-            tree[P] += (T - S + 1) * c;
-            B[P] += c;
-            return;
+        if (L <= S && R <= T) {
+            tree[P] += (R - S + 1) * c;
+            B[P] = c;
+
         }
-        int m = S + ((T - S) >> 1);
-        if (S != T && B[P] > 0) {
-            tree[P * 2] += (m - S + 1) * c;
-            tree[P * 2 + 1] += (T - m) * c;
-            B[P * 2] += B[P];
-            B[P * 2 + 1] += B[P];
-            B[P] = 0;
-        }
-        if (L <= m) {
-            update(tree, L, R, c, S, m, P * 2, B);
-        }
-        if (R > m) {
-            update(tree, L, R, c, m + 1, R, P * 2 + 1, B);
-        }
-        tree[P] = tree[P * 2] + tree[P * 2] + 1;
     }
 
     public void update01(int[] tree, int L, int R, int c, int S, int T, int p, int[] B) {
         if (L <= S && T <= R) {
-            tree[p] = (T - S + 1) * c;
-            B[p] = c;
+            tree[p] += (T - S + 1) * c;
+            B[p] += c;
             return;
         }
         int m = S + (T - S) >> 1;
-        if (B[p] > 0) {
-            tree[p * 2] = (m - S + 1) * B[p];
+        if (B[p] > 0 && S != T) {
+            B[p * 2] += (m - S + 1) * B[p];
+            B[p * 2 + 1] += (T - m) * B[p];
+            B[p * 2] += B[p];
+            B[p * 2 + 1] += B[p];
             B[p] = 0;
         }
-
-    }
-
-    public static void main(String[] args) {
-
+        if (L <= m) {
+            update(tree, L, R, c, S, m, p, B);
+        }
+        if (m < R) {
+            update(tree, L, R, c, m + 1, T, p, B);
+        }
+        tree[p] = tree[p * 2] + tree[p * 2 + 1];
     }
 }
