6ffb7884cf7cb8fcf6d4ee5717f3e10f2f5c8075
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-09-28 09:33:46 +0800

    leetcode 算法学习笔记

--- src/CF/Test01.java
+++ src/CF/Test01.java
@@ -8,11 +8,12 @@ package CF;
 public class Test01 {
     public static void main(String[] args) {
         int maxn = 100;
-        int[][] f = new int[maxn + 1][20];
-        int n = maxn;
+        int n = 100;
+        int[][] f = new int[maxn][21];
         for (int i = 1; i <= 20; i++) {
-            for (int j = 1; j + (1 << i) - 1 <= n; j++) {
-                f[j][i]=Math.max(f[j][i-1],f[j-(1<<(i-1))][i-1]);
+            for (int j = 1; j <= n; j++) {
+                f[j][i] = Math.max(f[j][i - 1], f[j + (1 << (i - 1))][i - 1]);
+
             }
 
         }
---
+++ src/luogu/P28/P2880BalancedLineupG.java
@@ -0,0 +1,9 @@
+package luogu.P28;
+
+/**
+ * @PackageName:luogu.P28
+ * @Date 2023/9/28 9:33
+ * @Description:
+ */
+public class P2880BalancedLineupG {
+}
