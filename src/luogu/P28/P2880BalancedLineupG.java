f7db0cca5a1b01ee4f6ee233617406e91e8c419b
xiang.li4 <wb_xiang.li4@sinitek.com>
2023-10-07 10:16:00 +0800

    leetcode 算法学习笔记

--- src/luogu/P28/P2880BalancedLineupG.java
+++ src/luogu/P28/P2880BalancedLineupG.java
@@ -9,6 +9,33 @@ import java.util.Scanner;
  */
 public class P2880BalancedLineupG {
 
+    public static void process() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int MAXN = 180010;
+        int[][]fmax=new int[MAXN][21];
+        int[][]fmin=new int[MAXN][21];
+        for (int i = 1; i <= n; i++) {
+            fmax[i][0]=sc.nextInt();
+            fmin[i][0]=fmax[i][0];
+        }
+        for (int i = 1; i < 21; i++) {
+            for (int j = 0; j+(1<<i) <=n; j++) {
+                fmax[j][i]=Math.max(fmax[j][i-1],fmax[j+(1<<(i-1))][i-1]);
+                fmin[j][i]=Math.min(fmin[j][i-1],fmin[j+(1<<(i-1))][i-1]);
+            }
+        }
+        for (int i = 0; i < q; i++) {
+            int L=sc.nextInt();
+            int R=sc.nextInt();
+            int s=(int)(Math.log(R-L+1)/Math.log(2));
+            int sMax=Math.max(fmax[L][s],fmax[R-(1<<(s-1))+1][s-1]);
+            int sMin=Math.min(fmin[L][s],fmin[R-(1<<(s-1))+1][s-1]);
+            System.out.println(sMax-sMin);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -29,9 +56,9 @@ public class P2880BalancedLineupG {
             int L = sc.nextInt();
             int R = sc.nextInt();
             int s = (int) (Math.log(R - L + 1) / Math.log(2));
-            int sMax=Math.max(fmax[L][s], fmax[R - (1 << s) + 1][s]);
-            int sMin=Math.min(fmin[L][s], fmin[R - (1 << s) + 1][s]);
-            System.out.println(sMax-sMin);
+            int sMax = Math.max(fmax[L][s], fmax[R - (1 << s) + 1][s]);
+            int sMin = Math.min(fmin[L][s], fmin[R - (1 << s) + 1][s]);
+            System.out.println(sMax - sMin);
         }
     }
 }
