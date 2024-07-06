ade3a1cb8a950a9d9dd04dcf0f007a6089a56042
nanji <1850094299@qq.com>
2024-07-07 00:06:43 +0800

    leetcode

--- src/zuo/base/class02/Code03_HeapSort.java
+++ src/zuo/base/class02/Code03_HeapSort.java
@@ -1,57 +1,61 @@
 package zuo.base.class02;
 
 
+import jdk.management.resource.internal.ResourceNatives;
+import utils.RandomUtils;
+
+import java.util.Random;
+
 public class Code03_HeapSort {
     public static void heapSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        //建立大根堆
         for (int i = 0; i < arr.length; i++) {
             heapInsert(arr, i);
         }
-        //排序
-        int heapSize = arr.length;
-        swap(arr, 0, --heapSize);
-        while (heapSize > 0) {
-            heapify(arr, 0, heapSize);
-            swap(arr, 0, --heapSize);
+        int size = arr.length;
+        RandomUtils.swap(arr, 0, --size);
+        while (size > 0) {
+            heapify(arr, 0, size);
+            RandomUtils.swap(arr, 0, --size);
+        }
+
+    }
+
+
+    private static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            RandomUtils.swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
         }
     }
 
     private static void heapify(int[] arr, int index, int heapSize) {
         int left = index * 2 + 1;
-        while (index < heapSize) {
+        while (left < heapSize) {
             int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
-            largest = arr[index] > arr[largest] ? index : largest;
-            if (largest == index) {
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (largest == index)
                 break;
-            }
-            swap(arr, largest, index);
+            RandomUtils.swap(arr, largest, index);
             index = largest;
             left = index * 2 + 1;
         }
-
     }
 
-    private static void heapInsert(int[] arr, int index) {
-        while (arr[(index - 1) / 2] < arr[index]) {
-            swap(arr, index, (index - 1) / 2);
-            index = (index - 1) / 2;
-        }
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        if (i == j) {
-            return;
-        }
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
-    }
 
     public static void main(String[] args) {
+        int[] arr = RandomUtils.generateRandomArray(100, 100);
+        int[] copyArr = RandomUtils.copyArray(arr);
+        heapSort(arr);
+        RandomUtils.sort(copyArr );
 
 
+
+        boolean equal = RandomUtils.isEqual(arr, copyArr);
+        RandomUtils.printArray(arr);
+        RandomUtils.printArray(copyArr);
+        System.out.println(equal);
     }
 }
