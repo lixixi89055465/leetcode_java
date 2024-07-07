890584563b129c6c998a116c370825217c974c9c
nanji <1850094299@qq.com>
2024-07-07 23:39:44 +0800

    leetcode

--- src/utils/RandomUtils.java
+++ src/utils/RandomUtils.java
@@ -44,10 +44,13 @@ public class RandomUtils {
 		if (arr == null) {
 			return;
 		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
+		if(arr.length==1){
+			System.out.println(arr[0]);
 		}
-		System.out.println();
+		for (int i = 0; i < arr.length-1; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		System.out.println(arr[arr.length-1]);
 	}
 	public static void swap(int[] arr,int i,int j) {
 		int tmp=arr[i];
