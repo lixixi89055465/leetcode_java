20e4702a364410360b363e773f84a1347960aef1
lixixi89055465 <nanji1850094299@outlook.com>
2024-07-08 14:42:43 +0800

    ZJ leetcode

--- src/utils/RandomUtils.java
+++ src/utils/RandomUtils.java
@@ -6,7 +6,7 @@ public class RandomUtils {
 	public static int[] generateRandomArray(int maxSize, int maxValue) {
 		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+			arr[i] = (int) ((maxValue + 1) * Math.random());
 		}
 		return arr;
 	}
