adf72749fe1803abc3caaaf71c8642490a0575be
lixixi89055465 <nanji1850094299@outlook.com>
2024-07-15 09:34:33 +0800

    ZJ leetcode

--- src/zuo/base/class04/Code04_IsPalindromeList.java
+++ src/zuo/base/class04/Code04_IsPalindromeList.java
@@ -15,49 +15,76 @@ public class Code04_IsPalindromeList {
         }
     }
 
-    public static boolean isPalindrome2(Node head) {
+    public static boolean isPalindrome3(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
         Node n1 = head;
         Node n2 = head;
-        while (n1.next != null && n2.next.next != null) {
+        while (n2.next != null && n2.next.next != null) {
             n1 = n1.next;
             n2 = n2.next.next;
         }
+        Node mid = n1;
         n2 = n1.next;
-        n1.next = null;
-        Node n3 = null;
+        Node tmp = null;
         while (n2 != null) {
-            n3 = n2.next;
+            tmp = n2.next;
             n2.next = n1;
             n1 = n2;
-            n2 = n3;
+            n2 = tmp;
         }
-        n3 = n1;
-        n2 = head;
-        boolean res = true;
-        while (n1 != null && n2 != null) {
-            if (n1.value != n2.value) {
-                res = false;
-                break;
+        Node tail = n1;
+        mid.next = null;
+        Node left = head;
+        Node right = n1;
+        while (left != right && left!=null) {
+            if (left.value != right.value) {
+                return false;
             }
-            n1 = n1.next;
-            n2 = n2.next;
+            left = left.next;
+            right = right.next;
         }
-        res = true;
-        n1 = n3.next;
-        n3.next = null;
-        while (n1 != null) {
-            n2 = n1.next;
-            n1.next = n3;
-            n3 = n1;
+        right = n1;
+        n2 = n1.next;
+        while (n2 != null) {
+            tmp = n2.next;
+            n2.next = n1;
             n1 = n2;
+            n2 = tmp;
         }
-        return res;
+        tail.next = null;
+        return true;
+    }
+
+    public static boolean isPalindrome2(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node right = head.next;
+        Node cur = head;
+        while (cur.next != null && cur.next.next != null) {
+            right = right.next;
+            cur = cur.next.next;
+        }
+        Stack<Node> stack = new Stack<>();
+        while (right != null) {
+            stack.push(right);
+            right = right.next;
+        }
+        while (!stack.isEmpty()) {
+            if (head.value != stack.pop().value) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
     }
 
     public static boolean isPalindrome1(Node head) {
         Stack<Node> stack = new Stack<>();
         Node cur = head;
-        while (cur != head) {
+        while (cur != null) {
             stack.push(cur);
             cur = cur.next;
         }
@@ -68,7 +95,26 @@ public class Code04_IsPalindromeList {
             head = head.next;
         }
         return true;
-
     }
 
+    public static void main(String[] args) {
+        Node n0 = new Node(0);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(3);
+        Node n5 = new Node(2);
+        Node n6 = new Node(1);
+        Node n7 = new Node(0);
+        n0.next = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = n7;
+        n7.next = null;
+        boolean r1 = isPalindrome3(n0);
+        System.out.println(r1);
+    }
 }
