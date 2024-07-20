fbf0b0262bd58c8c787550a47a24e36bf420de6a
lixixi89055465 <nanji1850094299@outlook.com>
2024-07-20 13:14:08 +0800

    zuo

--- src/zuo/base/class05/Code04_IsBST.java
+++ src/zuo/base/class05/Code04_IsBST.java
@@ -4,39 +4,52 @@ import java.util.LinkedList;
 
 public class Code04_IsBST {
 
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
 
-		public Node(int data) {
-			this.value = data;
-		}
-	}
+        public Node(int data) {
+            this.value = data;
+        }
+    }
 
-	public static boolean isBST(Node head) {
-		if (head == null) {
-			return true;
-		}
-		LinkedList<Node> inOrderList = new LinkedList<>();
-		process(head, inOrderList);
-		int pre = Integer.MIN_VALUE;
-		for (Node cur : inOrderList) {
-			if (pre >= cur.value) {
-				return false;
-			}
-			pre = cur.value;
-		}
-		return true;
-	}
+    public static int preValue = Integer.MIN_VALUE;
 
-	public static void process(Node node, LinkedList<Node> inOrderList) {
-		if (node == null) {
-			return;
-		}
-		process(node.left, inOrderList);
-		inOrderList.add(node);
-		process(node.right, inOrderList);
-	}
+    public static boolean checkBST(Node head) {
+        if (head == null) {
+            return true;
+        }
+        boolean isLeftBst = checkBST(head.left);
+        if (!isLeftBst) {
+            return false;
+        }
+        if (head.value < preValue) {
+            return false;
+        } else {
+            preValue = head.value;
+        }
+        return checkBST(head.right);
+    }
+
+    public static boolean isBST(Node head) {
+        if (head == null) {
+            return true;
+        }
+        boolean isLeftBst = isBST(head.left);
+        if (!isLeftBst) {
+            return false;
+        }else{
+            preValue=head.value;
+        }
+        return isBST(head.right);
+
+
+    }
+
+    public static void process(Node node, LinkedList<Node> inOrderList) {
+        if (node == null)
+            return;
+    }
 
 }
