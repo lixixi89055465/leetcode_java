e11e8433534ea262f843c42b1f6440c5f542f658
lixixi89055465 <nanji1850094299@outlook.com>
2024-07-29 19:35:50 +0800

    ZJ leetcode

--- src/zuo/base/class08/Code04_ReverseStackUsingRecursive.java
+++ src/zuo/base/class08/Code04_ReverseStackUsingRecursive.java
@@ -2,6 +2,10 @@ package zuo.base.class08;
 
 import java.util.Stack;
 
+/**
+ * 给你一个栈，请你逆序这个栈，不能申请额外的数据结构，只能使用递归函数。
+ * 如何实现?
+ */
 public class Code04_ReverseStackUsingRecursive {
 
 	public static void reverse(Stack<Integer> stack) {
--- src/zuo/base/class08/Code06_ConvertToLetterString.java
+++ src/zuo/base/class08/Code06_ConvertToLetterString.java
@@ -1,5 +1,9 @@
 package zuo.base.class08;
 
+/**
+ * 规定1和A对应、2和B对应、3和C对应... 那么一个数字字符串比如"111"，就可以转化为"AAA"、"KA"和"AK"。
+ * 给定一个只有数字字符组成的字符串str，返回有多少种转化结果。
+ */
 public class Code06_ConvertToLetterString {
 
 	public static int number(String str) {
