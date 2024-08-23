a3de6fb6e936b2237e87d8f3b59047072996f55e
lixixi89055465 <nanji1850094299@outlook.com>
2024-08-23 22:05:20 +0800

    zuo

--- src/zuo/intermediate/class01/Problem04_MaxOneBorderSize.java
+++ src/zuo/intermediate/class01/Problem04_MaxOneBorderSize.java
@@ -1,5 +1,16 @@
 package zuo.intermediate.class01;
 
+/**
+ * 给定一个N*N的矩阵matrix，只有0和1两种值，返回边框全是1的最大正方形的边
+ * 长长度。
+ * 例如:
+ * 01111
+ * 01001
+ * 01001
+ * 01111
+ * 01011
+ * 其中边框全是1的最大正方形的大小为4*4，所以返回4
+ */
 public class Problem04_MaxOneBorderSize {
 
     public static int f() {
