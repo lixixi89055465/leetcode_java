d1ec2ab31e55b7c092f67290cb0a5613ec256e51
nanji <1850094299@qq.com>
2024-08-25 17:26:00 +0800

    leetcode

--- src/zuo/intermediate/class03/Problem03_RotateMatrix.java
+++ src/zuo/intermediate/class03/Problem03_RotateMatrix.java
@@ -2,11 +2,17 @@ package zuo.intermediate.class03;
 
 
 /**
- * 用zigzag的方式打印矩阵，比如如下的矩阵
- * 0 1 2 3
- * 4 5 6 7
- * 8 9 10 11
- * 打印顺序为：0 1 4 8 5 2 3 6 9 10 7 11
+ 给定一个正方形矩阵，只用有限几个变量，实现矩阵中每个位置的数顺时针转动
+ 90度，比如如下的矩阵
+ 0 1 2 3
+ 4 5 6 7
+ 8 9 10 11
+ 12 13 14 15
+ 矩阵应该被调整为：
+ 12 8 4 0
+ 13 9 5 1
+ 14 10 6 2
+ 15 11 7 3
  */
 public class Problem03_RotateMatrix {
     public static void printMatrix(int[][] matrix) {
@@ -40,6 +46,13 @@ public class Problem03_RotateMatrix {
     }
 
     public static void main(String[] args) {
-
+        int[][] matrix = {
+                {0, 1, 2, 3},
+                {4, 5, 6, 7},
+                {8, 9, 10, 11},
+                {4, 5, 6, 7}
+        };
+        rotate(matrix);
+        printMatrix(matrix);
     }
 }
