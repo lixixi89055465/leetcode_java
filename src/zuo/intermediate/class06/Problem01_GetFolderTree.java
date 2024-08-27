f4b74da41e354165702c56def24f6de61f3e1b26
nanji <1850094299@qq.com>
2024-08-27 15:58:04 +0800

    leetcode

--- src/zuo/intermediate/class06/Problem01_GetFolderTree.java
+++ src/zuo/intermediate/class06/Problem01_GetFolderTree.java
@@ -2,6 +2,21 @@ package zuo.intermediate.class06;
 
 import java.util.TreeMap;
 
+/**
+ * 给你一个字符串类型的数组arr，譬如：
+ * String[] arr = { "b\\cst", "d\\", "a\\d\\e", "a\\b\\c" };
+ * 你把这些路径中蕴含的目录结构给画出来，子目录直接列在父目录下面，并比父目录
+ * 向右进两格，就像这样:
+ * a
+ * b
+ * c
+ * d
+ * e
+ * b
+ * cst
+ * d
+ * 同一级的需要按字母顺序排列，不能乱。
+ */
 public class Problem01_GetFolderTree {
 
 	public static class Node {
