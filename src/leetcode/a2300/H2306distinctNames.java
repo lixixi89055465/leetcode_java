0b5cded9837f464074049ece491bebc12982d2b9
lixixi89055465 <nanji1850094299@outlook.com>
2024-09-27 00:33:41 +0800

    leetcode

--- src/leetcode/a2300/H2306distinctNames.java
+++ src/leetcode/a2300/H2306distinctNames.java
@@ -1,5 +1,10 @@
 package leetcode.a2300;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -57,7 +62,36 @@ package leetcode.a2300;
 public class H2306distinctNames {
     private static class Solution {
         public long distinctNames(String[] ideas) {
-            return 0;
+            Map<Character, Set<String>> names = new HashMap<>();
+            for (String idea : ideas) {
+                names.putIfAbsent(idea.charAt(0), new HashSet<>());
+                names.get(idea.charAt(0)).add(idea.substring(1));
+            }
+            long ans = 0;
+            for (Map.Entry<Character, Set<String>> entryA : names.entrySet()) {
+                Character preA = entryA.getKey();
+                Set<String> setA = entryA.getValue();
+                for (Map.Entry<Character, Set<String>> entryB : names.entrySet()) {
+                    Character preB = entryB.getKey();
+                    Set<String> setB = entryB.getValue();
+                    if (preA == preB) {
+                        continue;
+                    }
+                    int intersect = getIntersectSize(setA, setB);
+                    ans += (long) (setA.size() - intersect) * (setB.size() - intersect);
+                }
+            }
+            return ans;
+        }
+
+        private int getIntersectSize(Set<String> a, Set<String> b) {
+            int ans = 0;
+            for (String s : a) {
+                if (b.contains(s)) {
+                    ans++;
+                }
+            }
+            return ans;
         }
     }
 
