56a07aa4c3217bdb004f2dd25f8f6ab5ef950d90
lixixi89055465 <nanji1850094299@outlook.com>
2024-11-08 19:37:24 +0800

    leetcode

---
+++ src/leetcode/a3200/H3235canReachCorner.java
@@ -0,0 +1,13 @@
+package leetcode.a3200;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： li
+ * @date： 2024/11/8 19:36
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class H3235canReachCorner {
+}
