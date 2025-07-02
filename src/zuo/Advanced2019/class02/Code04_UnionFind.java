1aeccc81ac7be00e2f4df8979f88cc09660308b9
lixixi89055465 <nanji1850094299@outlook.com>
2025-07-02 23:29:25 +0800

    209

--- src/zuo/Advanced2019/class02/Code04_UnionFind.java
+++ src/zuo/Advanced2019/class02/Code04_UnionFind.java
@@ -7,6 +7,13 @@ import java.util.Stack;
 
 public class
 Code04_UnionFind {
+    //    public static class Element<V> {
+//        public V value;
+//
+//        public Element(V value) {
+//            this.value = value;
+//        }
+//    }
     public static class Element<V> {
         public V value;
 
@@ -17,25 +24,26 @@ Code04_UnionFind {
 
     public static class UnionFindSet<V> {
         public HashMap<V, Element<V>> elementMap;
+        //key 某个元素 value 该元素的父
         public HashMap<Element<V>, Element<V>> fatherMap;
+        //key 某
         public HashMap<Element<V>, Integer> sizeMap;
 
         public UnionFindSet(List<V> list) {
             elementMap = new HashMap<>();
             fatherMap = new HashMap<>();
             sizeMap = new HashMap<>();
-            for (V value : list) {
-                Element<V> element = new Element<>(value);
-                elementMap.put(value, element);
+            for (V v : list) {
+                Element<V> element = new Element<>(v);
+                elementMap.put(v, element);
                 fatherMap.put(element, element);
                 sizeMap.put(element, 1);
             }
         }
 
         private Element<V> findHead(Element<V> element) {
-            Stack<Element<V>> path = new Stack<Element<V>>();
-
-            while (fatherMap.get(element) != element) {
+            Stack<Element<V>> path = new Stack<>();
+            while (element != fatherMap.get(element)) {
                 path.push(element);
                 element = fatherMap.get(element);
             }
@@ -52,21 +60,21 @@ Code04_UnionFind {
             return false;
         }
 
-        public void Union(V a, V b) {
-            if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
-                Element<V> heada = findHead(elementMap.get(a));
-                Element<V> headb = findHead(elementMap.get(b));
-                if (heada != headb) {
-                    Element<V> big = sizeMap.get(heada) >= sizeMap.get(headb) ? heada : headb;
-                    Element<V> small = sizeMap.get(heada) >= sizeMap.get(headb) ? headb : heada;
-                    fatherMap.put(small, big);
-                    sizeMap.put(big, sizeMap.get(heada) + sizeMap.get(headb));
-                    sizeMap.remove(small);
-                }
-
+        public void union(V a, V b) {
+            Element<V> ea = findHead(elementMap.get(a));
+            Element<V> eb = findHead(elementMap.get(b));
+            if (ea != eb) {
+                Element<V> big = sizeMap.get(ea) > sizeMap.get(eb) ? ea : eb;
+                Element<V> small = big == ea ? eb : ea;
+                fatherMap.put(small,big);
+                sizeMap.put(big,sizeMap.get(ea)+sizeMap.get(eb));
+                sizeMap.remove(small);
             }
+
+
         }
 
-
     }
+
+
 }
--- src/zuo/base/class08/Code03_PrintAllPermutations.java
+++ src/zuo/base/class08/Code03_PrintAllPermutations.java
@@ -1,5 +1,7 @@
 package zuo.base.class08;
 
+import utils.RandomUtils;
+
 import java.util.ArrayList;
 
 /**
@@ -7,32 +9,66 @@ import java.util.ArrayList;
  * 打印一个字符串的全部排列，要求不要出现重复的排列
  */
 public class Code03_PrintAllPermutations {
+    public static ArrayList<String> Permutation2(String str) {
+        ArrayList<String> res = new ArrayList<>();
+        if (str == null || str.length() == 0) {
+            return res;
+        }
+        char[] chs = str.toCharArray();
+        process01(chs, 0, res);
+        res.sort(null);
+        return res;
+    }
 
-	public static ArrayList<String> Permutation(String str) {
-		ArrayList<String> res = new ArrayList<>();
-		if (str == null || str.length() == 0) {
-			return res;
-		}
-		char[] chs = str.toCharArray();
-		process(chs, 0, res);
-		res.sort(null);
-		return res;
-	}
+    private static void process01(char[] chs, int i, ArrayList<String> res) {
+        if (i == chs.length) {
+            res.add(String.valueOf(chs));
+        }
+        //使用
+        process01(chs, i + 1, res);
+        //不使用
+        char tmp = chs[i];
+        chs[i] = 0;
+        process01(chs, i + 1, res);
+        chs[i] = tmp;
 
-	public static void process(char[] chs, int i, ArrayList<String> res) {
-		if (i == chs.length) {
-			res.add(String.valueOf(chs));
-		}
-		boolean[] visit = new boolean[26];
-		for (int j = i; j < chs.length; j++) {
-			if (!visit[chs[j] - 'a']) {
-				visit[chs[j] - 'a'] = true;
-				swap(chs, i, j);
-				process(chs, i + 1, res);
-				swap(chs, i, j);
-			}
-		}
-	}
+
+    }
+
+    public static ArrayList<String> Permutation(String str) {
+//		ArrayList<String> res = new ArrayList<>();
+//		if (str == null || str.length() == 0) {
+//			return res;
+//		}
+//		char[] chs = str.toCharArray();
+//		process(chs, 0, res);
+//		res.sort(null);
+//		return res;
+        ArrayList<String> res = new ArrayList<>();
+        if (str == null || str.length() == 0) {
+            return res;
+        }
+        char[] chs = str.toCharArray();
+        process(chs, 0, res);
+        res.sort(null);
+        return res;
+    }
+
+    public static void process(char[] chs, int i, ArrayList<String> res) {
+        if (i == chs.length) {
+            res.add(String.valueOf(chs));
+        }
+        boolean[] visit = new boolean[26];
+        for (int j = i; j < chs.length; j++) {
+            if (!visit[chs[j] - 'a']) {
+                visit[chs[j] - 'a'] = true;
+                RandomUtils.swap(chs, i, j);
+                process(chs, i + 1, res);
+                RandomUtils.swap(chs, i, j);
+            }
+
+        }
+    }
 
     public static void swap(char[] chs, int i, int j) {
         char tmp = chs[i];
