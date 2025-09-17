81b59899bf26f79849c43b1d89623c52fbfd30ff
lixixi89055465 <nanji1850094299@outlook.com>
2025-09-17 23:45:04 +0800

    init

--- src/zuo/senior/class01/Problem02_MostEOR.java
+++ src/zuo/senior/class01/Problem02_MostEOR.java
@@ -3,23 +3,42 @@ package zuo.senior.class01;
 import java.util.HashMap;
 
 public class Problem02_MostEOR {
+//    public static int mostEOR(int[] arr) {
+//        int xor = 0;
+//        int[] dp = new int[arr.length];
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        map.put(0, -1);
+//        for (int i = 0; i < arr.length; i++) {
+//            xor ^= arr[i];
+//            if (map.containsKey(xor)) {
+//                int pre = map.get(xor);
+//                dp[i] = pre == -1 ? 1 : (dp[pre] + 1);
+//            }
+//            if (i > 0) {
+//                dp[i] = Math.max(dp[i - 1], dp[i]);
+//            }
+//            map.put(xor, i);
+//        }
+//        return dp[arr.length - 1];
+//    }
+
     public static int mostEOR(int[] arr) {
-        int xor = 0;
-        int[] dp = new int[arr.length];
+        int xor=0;
+        int[]dp=new int[arr.length];
         HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
+        map.put(0,-1);
         for (int i = 0; i < arr.length; i++) {
-            xor ^= arr[i];
-            if (map.containsKey(xor)) {
-                int pre = map.get(xor);
-                dp[i] = pre == -1 ? 1 : (dp[pre] + 1);
+            xor^=arr[i];
+            if(map.containsKey(xor)){
+                int pre=map.get(xor);
+                dp[i]=pre==-1?1:(dp[pre]+1);
             }
-            if (i > 0) {
-                dp[i] = Math.max(dp[i - 1], dp[i]);
+            if(i>0){
+                dp[i]=Math.max(dp[i],dp[i-1]);
             }
-            map.put(xor, i);
+            map.put(xor,i);
         }
-        return dp[arr.length - 1];
+        return dp[dp.length-1];
     }
 
     public static void main(String[] args) {
